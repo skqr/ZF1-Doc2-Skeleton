5dc55611fd63998117f49e5979ed777537c8a002
Javier Lorenzana <javier.lorenzana@gointegro.com>
2012-11-12 13:47:16 -0300

    New User model.

---
+++ library/ZC/Entity/Proxy/User.php
@@ -0,0 +1,34 @@
+<?php 
+/**
+ *
+ */
+namespace ZC\Entity;
+/**
+ * 
+ * @Table(name="users")
+ * @author Javier Lorenzana <javier.lorenzana@gointegro.com>
+ */
+class User
+{
+    /**
+     *
+     * @var integer $id
+     * @Column(name="id", type="integer", nullable=false)
+     * @Id
+     * @GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+    /**
+     *
+     * @var integer $id
+     * @Column(type="string", length=60, nullable=true)
+     */
+    private $firstname;
+    /**
+     *
+     * @var integer $id
+     * @Column(type="string", length=60, nullable=true)
+     */
+    private $lastname;
+}
+?>
