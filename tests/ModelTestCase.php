82cf60831e0e161788ca2a3f4aa5fe029f1b41de
Javier Lorenzana <javier.lorenzana@gointegro.com>
2012-11-12 15:34:57 -0300

    Test for User.

--- tests/ModelTestCase.php
+++ tests/ModelTestCase.php
@@ -23,20 +23,20 @@ class ModelTestCase extends PHPUnit_Framework_TestCase
     	$application->bootstrap();
     	$this->doctrineContainer = Zend_Registry::get('doctrine');
     	$tool = new \Doctrine\ORM\Tools\SchemaTool($this->doctrineContainer->getEntityManager());
-    	$tool->createSchema(self::getClassMetas(APPLCATION_PATH . '/../library/ZC/Entity', 'ZC\Entity\\'));
+    	$tool->createSchema($this->getClassMetas(APPLCATION_PATH . '/../library/ZC/Entity', 'ZC\Entity\\'));
         parent::setUp();
     }
     /**
      *
      */
-    public static function getClassMetas($path, $namespace)
+    public function getClassMetas($path, $namespace)
     {
         $metas = array();
         if ($handle = opendir($path)) {
-			while (false !== ($file == readdir($handle))) {
+			while (false !== $file = readdir($handle)) {
 				if (strstr($file, '.php')) {
 					list($class) = explode('.', $file);
-					$metas[] = $this->doctrineContainer->getEntityManager()->getClassMetadata($class);
+					$metas[] = $this->doctrineContainer->getEntityManager()->getClassMetadata($namespace . $class);
 				}
 			}
         }
---
+++ tests/library/ZC/Entity/User.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *
+ */
+namespace ZC/Entity;
+/**
+ * Tests the functionality implemented in the class.
+ * @covers User
+ */
+class UserTest extends \ModelTestCase
+{
+    /**
+     * @covers User::__construct
+     */
+    public function testCanCreateUser()
+    {
+        $this->assertInstanceOf('ZC\Entity\User', new User());
+    }
+}
