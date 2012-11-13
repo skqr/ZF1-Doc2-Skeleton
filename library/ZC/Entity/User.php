5528a07c8663348c87662ad80a423c3ec3acea12
Javier Lorenzana <javier.lorenzana@gointegro.com>
2012-11-13 10:48:31 -0300

    Generic accessor and mutator for User.

--- library/ZC/Entity/User.php
+++ library/ZC/Entity/User.php
@@ -11,6 +11,10 @@ namespace ZC\Entity;
  */
 class User
 {
+    const ACCESSOR_PREFIX = 'get';
+    const ACCESSOR_PREFIX_LENGTH = 3;
+    const MUTATOR_PREFIX = 'set';
+    const MUTATOR_PREFIX_LENGTH = 3;
     /**
      *
      * @var integer $id
@@ -31,5 +35,24 @@ class User
      * @Column(type="string", length=60, nullable=true)
      */
     private $lastname;
+
+    /**
+     * Catches calls to undefined methods.
+     */
+    public function __call($method, $args)
+    {
+        if (self::ACCESSOR_PREFIX == substr($method, 0, self::ACCESSOR_PREFIX_LENGTH)) {
+            $property = lcfirst(substr($method, self::ACCESSOR_PREFIX_LENGTH));
+            if (property_exists(self, $property)) {
+                return $this->$property;
+            }
+        } elseif (self::MUTATOR_PREFIX == substr($method, 0, self::MUTATOR_PREFIX_LENGTH)) {
+            $property = lcfirst(substr($method, self::MUTATOR_PREFIX_LENGTH));
+            if (property_exists(self, $property)) {
+                $this->$property = $args[0];
+                return $this;
+            }
+        }
+    }
 }
 ?>
