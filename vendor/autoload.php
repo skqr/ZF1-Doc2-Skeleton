f97dd0e0076a6fa9e5a454e337b3f987748ffb63
Javier Lorenzana <javier.lorenzana@gointegro.com>
2012-11-14 14:53:18 -0300

    So I have to create a new behat.yml. Right.

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitf818069cd12fb917f83225fa81c109b5::getLoader();
+return ComposerAutoloaderInitfe385e93a90cad538748f08a7cd22414::getLoader();
