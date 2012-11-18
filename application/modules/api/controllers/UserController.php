9b18af7967623f414fcb6a9199db1fcb7111b682
Javier Lorenzana <javier.lorenzana@gointegro.com>
2012-11-18 16:44:56 -0300

    Turned our ZF app into a REST service with a single resource; User.

--- application/modules/api/controllers/UserController.php
+++ application/modules/api/controllers/UserController.php
@@ -1,18 +1,40 @@
 <?php
 
-class Api_UserController extends Zend_Controller_Action
+class Api_UserController extends Zend_Rest_Controller
 {
-
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
-
-    public function indexAction()
-    {
-        // action body
-    }
-
-
+	public function init()
+	{
+	    $this->_helper->viewRenderer->setNoRender(true);
+	}
+	
+	public function indexAction()
+	{
+	    $this->getResponse()->setBody('List of Resources');
+	    $this->getResponse()->setHttpResponseCode(200);
+	}
+	 
+	public function getAction()
+	{
+	    $this->getResponse()->setBody(sprintf('Resource #%s', $this->_getParam('id')));
+	    $this->getResponse()->setHttpResponseCode(200);
+	}
+	 
+	public function postAction()
+	{
+	    $this->getResponse()->setBody('Resource Created');
+	    $this->getResponse()->setHttpResponseCode(201);
+	}
+	 
+	public function putAction()
+	{
+	    $this->getResponse()->setBody(sprintf('Resource #%s Updated', $this->_getParam('id')));
+	    $this->getResponse()->setHttpResponseCode(201);
+	}
+	 
+	public function deleteAction()
+	{
+	    $this->getResponse()->setBody(sprintf('Resource #%s Deleted', $this->_getParam('id')));
+	    $this->getResponse()->setHttpResponseCode(200);
+	}
 }
 
