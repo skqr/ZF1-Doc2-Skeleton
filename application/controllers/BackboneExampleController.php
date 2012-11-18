5f8fd8940e3dcf7786ea49909f9ab29c29880db6
Javier Lorenzana <javier.lorenzana@gointegro.com>
2012-11-18 15:25:04 -0300

    Simple Backbone example found in http://arturadib.com/hello-backbonejs/

---
+++ application/controllers/BackboneExampleController.php
@@ -0,0 +1,42 @@
+<?php
+
+class BackboneExampleController extends Zend_Controller_Action
+{
+
+    /**
+     * An example using Backbone.js
+     */
+    public function indexAction()
+    {
+        /* Take a look at the template. */
+    }
+
+    /**
+     * This example illustrates the declaration and instantiation of a minimalist View.
+     */
+    public function example1Action()
+    {
+    }
+
+    /**
+     * This example illustrates the binding of DOM events to View methods.
+     */
+    public function example2Action()
+    {
+    }
+
+    /**
+     * This example illustrates how to use a Collection of Models to store data, and how to tie changes in those to a View.
+     */
+    public function example3Action()
+    {
+    }
+
+    /**
+     * This example introduces two new Model actions (swap and delete), illustrating how such actions can be handled within a Model's View.
+     */
+    public function example5Action()
+    {
+    }
+
+}
